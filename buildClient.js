65a90dde4ff12a2c9f51e6b72f5d759c6d942a49
Filip Smets <alemaaltevinden@gmail.com>
2015-07-25 01:06:27 +0200

    Add minifying to the building

--- buildClient.js
+++ buildClient.js
@@ -1,3 +1,4 @@
+var compressor = require('node-minify');
 var fs = require("fs");
 
 console.log("Listening to changes to client/scripts and client/css, will rebuild on file change.");
@@ -11,47 +12,70 @@ var rebuildJSTimeout,
 
 fs.watch("client/scripts", function (event) {
 	// Wait 100ms after the last change and reset if timer not reached
-	// Mose editors will do multiple edits for saving one file, this also
+	// Most editors will do multiple edits for saving one file, this also
 	// prevents ghost tmp files
 	clearTimeout(rebuildJSTimeout);
 	rebuildJSTimeout = setTimeout(rebuildJs, 100);
 });
 fs.watch("client/css", function (event) {
 	// Wait 100ms after the last change and reset if timer not reached
-	// Mose editors will do multiple edits for saving one file, this also
+	// Most editors will do multiple edits for saving one file, this also
 	// prevents ghost tmp files
 	clearTimeout(rebuildCssTimeout);
 	rebuildCssTimeout = setTimeout(rebuildCss, 100);
 });
 
+var rebuildingJs = false;
 function rebuildJs () {
-	fs.readdir("client/scripts", function (err, files) {
-		if (err) throw err;
+	// If we are already rebuilding, let it finish first
+	// then rebuild again
+	if (rebuildingJs) {
+		setTimeout(rebuildJs, 800);
+		return;
+	}
 
-		var minimizedFile = "";
-		for (var fKey = 0; fKey < files.length; fKey++) {
-			minimizedFile += fs.readFileSync("client/scripts/" + files[fKey], {encoding: "utf-8"}) + "\n";
-		}
+	console.log("[JS CHANGED] Rebuilding js files");
 
-		fs.writeFile("client/DrawTogether.min.js", minimizedFile, function (err) {
-			if (err) throw err;
+	rebuildingJs = true;
+	new compressor.minify({
+	    type: 'gcc',
+	    fileIn: "client/scripts/*.js",
+	    fileOut: 'client/DrawTogether.min.js',
+	    options: ["--language_in=ES5"],
+	    callback: function(err, min) {
+	   		rebuildingJs = false;
+	    	if (err) {
+	    		console.log("[ERROR] Rebuilding JS failed", err);
+	    		return;
+	    	}
 			console.log("JS FILE REBUILD");
-		});
+	    }
 	});
 }
 
+var rebuildingCss = false;
 function rebuildCss () {
-	fs.readdir("client/css", function (err, files) {
-		if (err) throw err;
+	// If we are already rebuilding, let it finish first
+	// then rebuild again
+	if (rebuildingCss) {
+		setTimeout(rebuildCss, 800);
+		return;
+	}
 
-		var minimizedFile = "";
-		for (var fKey = 0; fKey < files.length; fKey++) {
-			minimizedFile += fs.readFileSync("client/css/" + files[fKey], {encoding: "utf-8"}) + "\n";
-		}
+	console.log("[CSS CHANGED] Rebuilding css files");
 
-		fs.writeFile("client/DrawTogether.min.css", minimizedFile, function (err) {
-			if (err) throw err;
-			console.log("CSS FILE REBIULD");
-		});
+	rebuildingCss = true;
+	new compressor.minify({
+	    type: 'yui-css',
+	    fileIn: "client/css/*.css",
+	    fileOut: 'client/DrawTogether.min.css',
+	    callback: function(err, min) {
+	   		rebuildingCss = false;
+	    	if (err) {
+	    		console.log("[ERROR] Rebuilding CSS failed", err);
+	    		return;
+	    	}
+			console.log("CSS FILE REBUILD");
+	    }
 	});
 }
