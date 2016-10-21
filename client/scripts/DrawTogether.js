eccee07bfca74a6b3a16ae5a026d5bf01f01c583
intOrfloat <nohup.or.disown@gmail.com>
2016-10-20 23:08:43 -0700

    added save bug feature fix to feature window

--- client/scripts/DrawTogether.js
+++ client/scripts/DrawTogether.js
@@ -2870,7 +2870,8 @@ DrawTogether.prototype.openNewFeatureWindow = function openNewFeatureWindow () {
 
 	var ol = container.appendChild(document.createElement("ol"));
 
-	var features = ["It is now possible to manage regions and their permissions.",
+	var features = ["Fixed bug that prevented lines from being saved",
+					"It is now possible to manage regions and their permissions.",
 					"Anyone with 30R+ can make a single protected region now! The region size limit is scaled with rep."];
 	for (var k = 0; k < features.length; k++) {
 		var li = ol.appendChild(document.createElement("li"));
