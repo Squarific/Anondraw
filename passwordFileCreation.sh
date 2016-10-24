2433195294c804331939ffb38082b180957700c1
intOrfloat <nohup.or.disown@gmail.com>
2016-10-24 00:34:56 -0700

    added _password.js wizard

---
+++ passwordFileCreation.sh
@@ -0,0 +1,27 @@
+echo Password file creation script...
+
+echo Would you like to continue? y/n
+
+read allCool
+
+if [ "$allCool" = "y" ]; then
+	echo enter mysql password: 
+	read mysqlp
+	echo $mysqlp > server/playerServer/mysql_password.js
+
+	echo enter password for everything else: 
+	read password
+
+	echo $password | tee server/imageServer/draw_password.js server/playerServer/status_password.js server/playerServer/kickban_password.js server/realTime/draw_password.js server/realTime/scripts/kickban_password.js server/realTime/join_code_password.js server/realTime/imgur_password.js server/loadBalancer/status_password.js server/loadBalancer/join_code_password.js
+fi
+
+#./server/imageServer/draw_password.js
+#./server/playerServer/status_password.js
+#./server/playerServer/kickban_password.js
+#./server/playerServer/mysql_password.js
+#./server/realTime/draw_password.js
+#./server/realTime/scripts/kickban_password.js
+#./server/realTime/join_code_password.js
+#./server/realTime/imgur_password.js
+#./server/loadBalancer/status_password.js
+#./server/loadBalancer/join_code_password.js
