7c6391095da3e1b62bdee43cd7cb42d2eb58c672
intOrfloat <nohup.or.disown@gmail.com>
2016-10-24 00:43:38 -0700

    added quotes to formatting of passwords

--- passwordFileCreation.sh
+++ passwordFileCreation.sh
@@ -7,13 +7,14 @@ read allCool
 if [ "$allCool" = "y" ]; then
 	echo enter mysql password: 
 	read mysqlp
-	
-	echo module.exports = $mysqlp\; | tee server/playerServer/mysql_password.js
+
+	echo module.exports = '"'$mysqlp'";' | tee server/playerServer/mysql_password.js
 
 	echo enter password for everything else: 
 	read password
 
-	echo module.exports = $password\; | tee server/imageServer/draw_password.js server/playerServer/status_password.js server/playerServer/kickban_password.js server/realTime/draw_password.js server/realTime/scripts/kickban_password.js server/realTime/join_code_password.js server/realTime/imgur_password.js server/loadBalancer/status_password.js server/loadBalancer/join_code_password.js
+	#module.exports = "yourpassword";
+	echo module.exports = '"'$password'";' | tee server/imageServer/draw_password.js server/playerServer/status_password.js server/playerServer/kickban_password.js server/realTime/draw_password.js server/realTime/scripts/kickban_password.js server/realTime/join_code_password.js server/realTime/imgur_password.js server/loadBalancer/status_password.js server/loadBalancer/join_code_password.js
 fi
 
 #./server/imageServer/draw_password.js
