970536fff88d08430081673016a5152b56322161
Filip Smets <alemaaltevinden@gmail.com>
2016-11-17 14:37:59 +0100

    Add another query

--- tools/queries.sql
+++ tools/queries.sql
@@ -5,3 +5,12 @@ from users as u
         on r.to_id = u.id
 group by u.id
 order by reputation desc ;
+
+-- Accountbans with last_username, startdate, enddate and reputation
+
+SELECT last_username, startdate, enddate, COUNT(to_id)
+FROM accountbans
+    JOIN users ON accountbans.userid = users.id
+    JOIN reputations ON accountbans.userid = reputations.to_id
+GROUP BY to_id
+ORDER BY startdate;
