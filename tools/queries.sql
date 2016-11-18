2537ed8a3687654aecb8dac87ce8bb695ccb2c75
Filip Smets <alemaaltevinden@gmail.com>
2016-11-18 12:32:23 +0100

    Add more info to the ban query

--- tools/queries.sql
+++ tools/queries.sql
@@ -6,11 +6,16 @@ from users as u
 group by u.id
 order by reputation desc ;
 
--- Accountbans with last_username, startdate, enddate and reputation
-
-SELECT last_username, startdate, enddate, COUNT(to_id)
+-- Accountbans with last_username, startdate, enddate, reputation, banned_by, banner.last_username and reason
+SELECT
+    users.last_username,
+    startdate, enddate,
+    COUNT(to_id) as rep,
+    banned_by, banner.last_username,
+    reason
 FROM accountbans
-    JOIN users ON accountbans.userid = users.id
-    JOIN reputations ON accountbans.userid = reputations.to_id
-GROUP BY to_id
+    JOIN users ON accountbans.userid = users.id 
+    JOIN reputations ON accountbans.userid = reputations.to_id 
+    JOIN users AS banner ON banned_by = banner.id
+GROUP BY to_id, startdate
 ORDER BY startdate;
