a800b46f7cba8d29501a45c936aa0fba58636a28
Filip Smets <alemaaltevinden@gmail.com>
2016-12-01 18:18:20 +0100

    Add a trigger for the referral check

--- src/server/playerServer/database.sql
+++ src/server/playerServer/database.sql
@@ -43,6 +43,30 @@ CREATE TABLE IF NOT EXISTS reputations (
 --For the possible source values, see PlayerDatabase.js
 ALTER TABLE reputations ADD COLUMN source INT UNSIGNED DEFAULT 0;
 
+CREATE TRIGGER referralcheck
+AFTER INSERT
+    ON reputations FOR EACH ROW
+BEGIN
+    INSERT INTO
+        reputations (from_id, to_id, source)
+    SELECT
+        triggereduser.id as from_id,
+        triggereduser.referral as to_id,
+        2 as source
+    FROM users AS triggereduser
+    INNER JOIN reputations
+        ON triggereduser.id = reputations.to_id
+    GROUP BY reputations.to_id
+    HAVING COUNT(reputations.to_id) > 10
+	AND EXISTS (SELECT * FROM users WHERE id = triggereduser.referral)
+        AND NOT EXISTS (
+            SELECT * FROM reputations
+            WHERE from_id = 1
+                AND to_id = triggereduser.referral
+                AND source = 2
+	);
+END;
+
 CREATE TABLE IF NOT EXISTS ipbans (
     ip VARCHAR(48),
     banned_by INT UNSIGNED,
