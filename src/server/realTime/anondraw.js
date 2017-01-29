f67cd22bec843132fbce747d0c35e6e7a5fb3ad6
intOrfloat <nohup.or.disown@gmail.com>
2017-01-29 17:43:49 -0500

    removed log

--- src/server/realTime/anondraw.js
+++ src/server/realTime/anondraw.js
@@ -78,10 +78,6 @@ function saveAndShutdown () {
 		return protocol.getUserCount(roomNameB) - protocol.getUserCount(roomNameA);
 	}.bind(this));
 	
-	for (var k = 0; k < rooms.length; k++) {
-		console.log("room " + rooms[k], "Has " +  protocol.getUserCount(rooms[k]) + "people");
-	}
-	
 	var attempts = 0;
 	var index = rooms.length - 1;
 	
