9cc3b1fef027c5ed8fa30a96b2477a1206c6b8e8
Filip Smets <alemaaltevinden@gmail.com>
2017-05-17 14:34:12 +0200

    Fix ambigious order by in query

--- src/server/playerServer/scripts/MessageDatabase.js
+++ src/server/playerServer/scripts/MessageDatabase.js
@@ -12,7 +12,7 @@ MessageDatabase.prototype.addMessage = function addMessage (userId, to, message,
 };
 
 MessageDatabase.prototype.getMessageList = function getMessageList (userId, callback) {
-	this.database.query("SELECT partner, last_username, CAST(MIN(isRead) AS INT) as isRead FROM (SELECT toId as partner, 1 as isRead FROM messages WHERE fromId = ? ORDER BY send DESC UNION ALL SELECT fromId as partner, isRead FROM messages WHERE toId = ? ORDER BY send DESC) as partners JOIN users ON partners.partner = users.id GROUP BY partner", [userId, userId], function (err, rows, fields) {
+	this.database.query("SELECT partner, last_username, CAST(MIN(isRead) AS INT) as isRead FROM ((SELECT toId as partner, 1 as isRead FROM messages WHERE fromId = ? ORDER BY send DESC) UNION ALL (SELECT fromId as partner, isRead FROM messages WHERE toId = ? ORDER BY send DESC)) as partners JOIN users ON partners.partner = users.id GROUP BY partner", [userId, userId], function (err, rows, fields) {
 		callback(err, rows);
 	});
 };
