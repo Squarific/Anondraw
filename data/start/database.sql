74c3f2b1f08802c1258913de5fbe03dd4242372a
Filip Smets <filipsmetsbelgium@gmail.com>
2021-05-10 18:41:26 +0200

    Add uuid field to db

--- data/start/database.sql
+++ data/start/database.sql
@@ -13,6 +13,37 @@ ALTER TABLE users ADD COLUMN last_online DATETIME;
 ALTER TABLE users ADD COLUMN register_datetime DATETIME;
 ALTER TABLE users ADD COLUMN headerImage VARCHAR(128);
 ALTER TABLE users ADD COLUMN profileImage VARCHAR(128);
+ALTER TABLE users ADD COLUMN uuid BINARY(16) DEFAULT UUID();
+
+DELIMITER //
+
+CREATE FUNCTION UuidToBin(_uuid BINARY(36))
+    RETURNS BINARY(16)
+    LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+RETURN
+    UNHEX(CONCAT(
+        SUBSTR(_uuid, 15, 4),
+        SUBSTR(_uuid, 10, 4),
+        SUBSTR(_uuid,  1, 8),
+        SUBSTR(_uuid, 20, 4),
+        SUBSTR(_uuid, 25) ));
+//
+CREATE FUNCTION UuidFromBin(_bin BINARY(16))
+    RETURNS BINARY(36)
+    LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+RETURN
+    LCASE(CONCAT_WS('-',
+        HEX(SUBSTR(_bin,  5, 4)),
+        HEX(SUBSTR(_bin,  3, 2)),
+        HEX(SUBSTR(_bin,  1, 2)),
+        HEX(SUBSTR(_bin,  9, 2)),
+        HEX(SUBSTR(_bin, 11))
+             ));
+
+//
+DELIMITER ;
+    
+CREATE TRIGGER before_insert_anondrawusers BEFORE INSERT ON users FOR EACH ROW SET new.uuid = UuidToBin(uuid());
 
 CREATE TABLE IF NOT EXISTS teams (
 	id INT UNSIGNED AUTO_INCREMENT,
