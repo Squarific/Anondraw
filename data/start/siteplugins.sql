04fbb6fb0baef648b2c6a07edf61f2471cf67932
Ilger Depraetere <ilgerdepraetere@gmail.com>
2021-05-12 20:27:57 +0200

    commemnt out create database for siteplugins

--- data/start/siteplugins.sql
+++ data/start/siteplugins.sql
@@ -1,33 +1,33 @@
-CREATE DATABASE IF NOT EXISTS `siteplugins`;
-USE `siteplugins`;
+-- CREATE DATABASE IF NOT EXISTS `siteplugins`;
+-- USE `siteplugins`;
 
-DELIMITER //
+-- DELIMITER //
 
-CREATE FUNCTION UUID_TO_BIN(_uuid BINARY(36))
-    RETURNS BINARY(16)
-    LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
-RETURN
-    UNHEX(CONCAT(
-        SUBSTR(_uuid, 15, 4),
-        SUBSTR(_uuid, 10, 4),
-        SUBSTR(_uuid,  1, 8),
-        SUBSTR(_uuid, 20, 4),
-        SUBSTR(_uuid, 25) ));
-//
-CREATE FUNCTION BIN_TO_UUID(_bin BINARY(16))
-    RETURNS BINARY(36)
-    LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
-RETURN
-    LCASE(CONCAT_WS('-',
-        HEX(SUBSTR(_bin,  5, 4)),
-        HEX(SUBSTR(_bin,  3, 2)),
-        HEX(SUBSTR(_bin,  1, 2)),
-        HEX(SUBSTR(_bin,  9, 2)),
-        HEX(SUBSTR(_bin, 11))
-             ));
+-- CREATE FUNCTION UUID_TO_BIN(_uuid BINARY(36))
+--     RETURNS BINARY(16)
+--     LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+-- RETURN
+--     UNHEX(CONCAT(
+--         SUBSTR(_uuid, 15, 4),
+--         SUBSTR(_uuid, 10, 4),
+--         SUBSTR(_uuid,  1, 8),
+--         SUBSTR(_uuid, 20, 4),
+--         SUBSTR(_uuid, 25) ));
+-- //
+-- CREATE FUNCTION BIN_TO_UUID(_bin BINARY(16))
+--     RETURNS BINARY(36)
+--     LANGUAGE SQL  DETERMINISTIC  CONTAINS SQL  SQL SECURITY INVOKER
+-- RETURN
+--     LCASE(CONCAT_WS('-',
+--         HEX(SUBSTR(_bin,  5, 4)),
+--         HEX(SUBSTR(_bin,  3, 2)),
+--         HEX(SUBSTR(_bin,  1, 2)),
+--         HEX(SUBSTR(_bin,  9, 2)),
+--         HEX(SUBSTR(_bin, 11))
+--              ));
 
-//
-DELIMITER ;
+-- //
+-- DELIMITER ;
 
 
 CREATE TABLE IF NOT EXISTS `plugins` (
