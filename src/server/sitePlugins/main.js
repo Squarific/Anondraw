ff653b9d5f7fbb6857db71780f075224e2a66f0d
Ilger Depraetere <ilgerdepraetere@gmail.com>
2021-05-12 18:59:59 +0200

    add https to main.js sitePlugins

--- src/server/sitePlugins/main.js
+++ src/server/sitePlugins/main.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const app = express()
+const https = require('https')
 const cors = require('cors')
+const app = express()
 
-var mysql = require('mysql');
-var pool = mysql.createPool({
+const config = require("../common/config.js");
+
+const mysql = require('mysql');
+const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
   user: 'pluginmods',
@@ -12,6 +15,12 @@ var pool = mysql.createPool({
   multipleStatements: true
 });
 
+const options = {
+  key: fs.readFileSync(config.permfolder + '/privkey.pem'),
+  cert: fs.readFileSync(config.permfolder + '/cert.pem'),
+  ca: fs.readFileSync(config.permfolder + '/chain.pem')
+};
+
 app.use(express.json());
 app.use(cors())
 
@@ -19,6 +28,6 @@ app.use('/plugins', require('./routes/plugins/_')(pool));
 app.use('/versions', require('./routes/versions/_')(pool));
 
 const PORT = 6552;
-app.listen(PORT, '0.0.0.0', () => {
+https.createServer(options, app).listen(PORT, '0.0.0.0', () => {
   console.log("Listening on " + PORT + "...");
 });
